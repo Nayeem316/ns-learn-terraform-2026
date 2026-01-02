34470ed57cc1efab1c13274cb55114f9d0ac6d30
Nayeem Syed <syed.nayeem316@gmail.com>
2026-01-01 22:14:14 -0500

    VPC ans Subnet created

--- resources.tf
+++ resources.tf
@@ -7,3 +7,27 @@ resource "aws_vpc" "development-vpc" {
     }
   )  
 }
+
+resource "aws_subnet" "development-subnet-1" {
+  vpc_id            = aws_vpc.development-vpc.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "${var.aws_region}a"
+  tags = merge(
+    local.common_tags,
+    {
+      Name = "${var.name_prefix}-subnet-1"
+    }
+  )
+}
+
+resource "aws_subnet" "development-subnet-2" {
+  vpc_id            = aws_vpc.development-vpc.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "${var.aws_region}b"
+  tags = merge(
+    local.common_tags,
+    {
+      Name = "${var.name_prefix}-subnet-2"
+    }
+  )
+}
