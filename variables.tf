fb1c2d1931346223cc2e8244ec91cc6538126c38
Nayeem Syed <syed.nayeem316@gmail.com>
2026-01-01 21:59:56 -0500

    my-project

--- variables.tf
+++ variables.tf
@@ -11,7 +11,7 @@ variable "environment" {
 variable "aws_region" {
   description = "AWS region"
   type        = string
-  default     = "us-east-1"
+  default     = "us-east-2"
 }
 
 variable "additional_tags" {
@@ -20,6 +20,3 @@ variable "additional_tags" {
   default     = {}
 }
 
-variable "vpc_name" {
-  type = string
-}
